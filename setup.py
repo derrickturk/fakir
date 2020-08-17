132dc6092aa058915e19c6f222c1d10d67ae2089
Derrick W. Turk <dwt@terminusdatascience.com>
2020-08-17 10:24:05 -0500

    setup tweaks for pypi

--- setup.py
+++ setup.py
@@ -7,13 +7,17 @@ setup(
     name='fakir',
     description='a mildly monadic module for fast faking',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Derrick W. Turk',
     url='https://github.com/derrickturk/fakir',
     version='0.1',
     py_modules=['fakir'],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
     ],
+    python_requires='>=3.6',
 )
